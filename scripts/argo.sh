e1b9018da8dd319883c7d030ae960630e718f9f1
Stoica Tedy <stoicatedy@gmail.com>
2019-10-23 16:37:10 +0300

    asd

--- scripts/argo.sh
+++ scripts/argo.sh
@@ -1,2 +1,4 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/install.yaml
