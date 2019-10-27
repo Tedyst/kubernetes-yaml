25010cc0d36262a1e8e1a5a61971c097a5e6381f
Stoica Tedy <stoicatedy@gmail.com>
2019-10-27 16:29:05 +0200

    asd

--- scripts/argo.sh
+++
@@ -1,4 +0,0 @@
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl create namespace argo
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/install.yaml
--- scripts/networking.sh
+++
@@ -1,1 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
